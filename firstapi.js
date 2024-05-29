7058ce0fd7641f46fde5f22d733a4ef8c2cba6f4
nagaraja6050266 <nagaraja6050266@gmail.com>
2024-05-29 14:30:30 +0530

    After adding .env

--- firstapi.js
+++ firstapi.js
@@ -1,41 +1,33 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const Product=require('./models/product.model.js');
-const productRoute=require('./routes/product.route.js');
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const path = require("path");
 
 //Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.use('/api/products',productRoute);
+app.use("/api/products", productRoute);
 
+mongoose
+    .connect(process.env.MONGODB_URI,{
+        dbName: "Products"
+    })
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch(() => {
+        console.log("Not Connected ");
+    });
 
-mongoose.connect("mongodb://localhost:27017")
-.then(() => {
-    console.log("Connected to DB");
-})
-.catch(() => {
-    console.log("Not Connected ");
-})
-
-app.listen(3001,() => {
+app.listen(3001, () => {
     console.log("Server is running in 3001");
 });
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to the simple CRUD API");
-})
-
-//Read Every producs
-
-//Read Particular Product using id
-
-
-//Update a product
-
-
-//Deleting a product
+app.get("/", (req, res) => {
+    res.send("Welcome To Simple CRUD API");
+});
